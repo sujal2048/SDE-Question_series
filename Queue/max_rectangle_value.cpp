6bc2c7cd14335197f106a03782ee768bf0981203
Sujal Gupta <75678206+sujal2048@users.noreply.github.com>
2026-03-24 00:23:31 +0530

    Update max_rectangle_value.cpp

--- Queue/max_rectangle_value.cpp
+++ Queue/max_rectangle_value.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>           //https://www.youtube.com/watch?v=hGl8Boeb7S4&t=49
 
 using namespace std;
-int get_max_area(vector<int> a){                //if it is increasing let it be but if it is decreasing then check the 
+int get_max_area(vector<int> a){                //if it is incre 
     int n=a.size(),ans=0,i=0;               //potential rectangle then further
     a.push_back(0);
     stack <int> st;
