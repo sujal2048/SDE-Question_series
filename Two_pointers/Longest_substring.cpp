4a64249d5df6ea804f6695f90d027f3b26d7f3e9
Sujal Gupta <75678206+sujal2048@users.noreply.github.com>
2026-03-30 01:20:15 +0530

    Update dictionary with current index in substring search
    Fix logic to update the dictionary with the current index.

--- Two_pointers/Longest_substring.cpp
+++ Two_pointers/Longest_substring.cpp
@@ -7,7 +7,6 @@ int main(){
     int maxlen=0,start=-1;
     for(int i=0;i<s.size();i++){
           start=dict[s[i]];
-        dict[s[i]]=i;
         maxlen = max(maxlen,i-start);
     }
     cout<<maxlen;
