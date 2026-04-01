3ebd95e98659974a667cda758d763e45be61c00e
Sujal Gupta <75678206+sujal2048@users.noreply.github.com>
2026-04-02 00:13:27 +0530

    Correctly mark non-prime numbers in sieve algorithm
    Fix the sieve algorithm to mark non-prime numbers correctly.

--- array/sieve_of_erathues.cpp
+++ array/sieve_of_erathues.cpp
@@ -5,7 +5,7 @@ void primsieve(int n){
     for(int i=2;i<=n;i++){
         if(prime[i]==0){
             for(int j=i*i;j<=n;j+=i){
-                prime[j]=1;
+       
             }
         }
     }
