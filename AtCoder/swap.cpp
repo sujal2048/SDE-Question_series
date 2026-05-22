de25b679cfb8aa9c567ee3d751a71ca65a5bf9e6
Sujal Gupta <75678206+sujal2048@users.noreply.github.com>
2026-05-23 00:28:30 +0530

    Refactor nested loop condition in swap.cpp
    Removed unnecessary condition check and variable assignment in nested loop.

--- AtCoder/swap.cpp
+++ AtCoder/swap.cpp
@@ -20,9 +20,7 @@ int main(){
         int mx=INT_MAX;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(a[i]<b[j] && mx>(i+j)){
-                    mx;
-                }
+           
                 if(i+j>mx){
                     break;
                    
