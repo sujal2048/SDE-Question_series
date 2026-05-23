20a9074786176cdcb6e2e44410803e553d341954
Sujal Gupta <75678206+sujal2048@users.noreply.github.com>
2026-05-24 00:17:32 +0530

    Correct loop increment in climbing stairs function
    Fix the loop increment in climbing_stairs.cpp.

--- Dynamic_programming/climbing_stairs.cpp
+++ Dynamic_programming/climbing_stairs.cpp
@@ -6,7 +6,7 @@ int main(){                    //iterative approach using dynamic programming
      a[0]=1;                            
   
      for(int i=2;i<=n;i++){
-         a[i]=a[i-1]+a[i-2];
+         a[i]=a[i-1]
      }
      cout<<a[n]<<endl;
 }
